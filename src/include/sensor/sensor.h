74e1cc5fa16c833be7f7a8901550b7b0d224ced4
edwin7026 <edwin.joy@incoresemi.com>
2022-11-21 18:55:50 +0530

    Sensor module initial commit

---
+++ src/include/sensor/sensor.h
@@ -0,0 +1,22 @@
+#include <iostream>
+
+/**
+ * @class This class holds the functionality of the IMU sensor
+ **/
+class sensor
+{
+    // Data members
+
+    // Accelerometer
+    float acc_out[3];
+
+    // Gyroscope
+    float gyro_out[3];
+
+    // Magnetometer
+    float mag_out[3];
+
+
+
+
+};
